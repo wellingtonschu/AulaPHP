6a85cf081dfed4f12ba58008abfd65c49c3efe0a
Wellington Schütz <wellington.schu@live.com>
2016-07-12 18:23:08 -0300

    Aula 04

--- Aula04/tipos.php
+++ Aula04/tipos.php
@@ -2,14 +2,20 @@
 <html>
     <head>
         <meta charset="UTF-8"/>
+        <link rel="stylesheet" href="_css/estilo.css"/>
         <title></title>
     </head>
     <body>
-        <?php
-            $n = 4;
-            $no = "Wellington";
-
-            echo $no;
-        ?>
+        <div>
+            <?php
+                $nome = "Wellington";
+                $idade = 25;
+                echo $nome." tem ".$idade." anos.";
+                echo "</br>";
+                echo "$nome tem $idade anos.";
+                echo "</br>";
+                echo '$nome tem $idade anos.';
+            ?>
+        </div>
     </body>
 </html>
